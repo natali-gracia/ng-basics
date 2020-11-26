3b61180c54bc8197ea9791a341b31110de922bac
Natalia Smalii <natali1gracia@gmail.com>
2020-11-26 16:51:43 +0200

    created simple pipe with cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
 import { AppComponent } from './app.component'
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { ExMarksPipe } from './pipes/ex-marks.pipe'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ExMarksPipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
