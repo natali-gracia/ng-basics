63be4714134520e0d704de99a3fdf89f5ae6edb6
Natalia Smalii <natali1gracia@gmail.com>
2020-11-26 13:03:58 +0200

    clear starter project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,8 @@
-import { Component, OnInit } from '@angular/core'
-
-export interface Post {
-  title: string
-  text: string
-  id?: number
-}
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  posts: Post[] = [
-    {
-      title: 'Create unbelievable Component',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      id: 1,
-    },
-    {
-      title: 'Use best practice in development',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      id: 2,
-    },
-  ]
-
-  ngOnInit(): void {
-    setTimeout(() => {
-      console.log('Timeout')
-      // this.posts[0].title = 'Changed!'
-      this.posts[0] = {
-        title: 'Changed!',
-        text: 'Change text 42',
-        id: 42,
-      }
-    }, 5000)
-  }
-
-  updatePosts(post: Post) {
-    this.posts.unshift(post)
-  }
-
-  removePost(id: number) {
-    console.log('Id to remove', id)
-    this.posts = this.posts.filter((post) => post.id !== id)
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core'
 
 import { AppComponent } from './app.component'
 import { FormsModule } from '@angular/forms'
-import { PostFormComponent } from './post-form/post-form.component'
-import { PostComponent } from './post/post.component'
 import { CommonModule } from '@angular/common'
 
 @NgModule({
-  declarations: [AppComponent, PostFormComponent, PostComponent],
+  declarations: [AppComponent],
   imports: [CommonModule, BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
