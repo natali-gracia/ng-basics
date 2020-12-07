c32652d67c0d385b92d7537e6cc099a11c4c1e65
Natalia Smalii <natali1gracia@gmail.com>
2020-12-07 15:27:03 +0200

    add metatags for seo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ComponentFactoryResolver, ViewChild } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { ModalComponent } from './modal/modal.component';
 import { RefDirective } from './ref.directive';
 
@@ -10,7 +11,17 @@ import { RefDirective } from './ref.directive';
 export class AppComponent {
   @ViewChild(RefDirective) refDir!: RefDirective;
 
-  constructor(private resolver: ComponentFactoryResolver) {}
+  constructor(
+    private resolver: ComponentFactoryResolver,
+    private title: Title,
+    private meta: Meta
+  ) {
+    title.setTitle('Angular basics');
+    meta.addTags([
+      { name: 'keywords', content: 'angular, education, course, basic' },
+      { name: 'description', content: 'This is Angular Basics course' },
+    ]);
+  }
 
   showModal() {
     const modalFactory = this.resolver.resolveComponentFactory(ModalComponent);
