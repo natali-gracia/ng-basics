39d516114e1361d6e86b6ffa162146e8809943b8
Natalia Smalii <natali1gracia@gmail.com>
2020-12-07 12:40:06 +0200

    create dynamic-component with viewcontainerref

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewChild } from '@angular/core';
+import { ModalComponent } from './modal/modal.component';
+import { RefDirective } from './ref.directive';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  modal = false;
+  @ViewChild(RefDirective) refDir!: RefDirective;
+
+  constructor(private resolver: ComponentFactoryResolver) {}
+
+  showModal() {
+    const modalFactory = this.resolver.resolveComponentFactory(ModalComponent);
+    // this.refDir.containerRef.clear();
+    const component = this.refDir.containerRef.createComponent(modalFactory);
+    component.instance.title = 'Dynamic title';
+    component.instance.close.subscribe(() => {
+      this.refDir.containerRef.clear();
+    });
+  }
 }
--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,18 +1,15 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core'
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent implements OnInit {
+  @Input() title = 'Default title';
+  @Output() close = new EventEmitter<void>();
 
-  @Input() title = 'Default title'
-  @Output() close = new EventEmitter<void>()
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
