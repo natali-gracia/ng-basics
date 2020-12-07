ec72ee7c19e5d72305f592d4bb044be9ebc882bb
Natalia Smalii <natali1gracia@gmail.com>
2020-12-07 11:21:31 +0200

    created enviroment for dynamic-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component'
-import { FormsModule } from '@angular/forms'
-import { CommonModule } from '@angular/common'
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [CommonModule, BrowserModule, FormsModule],
+  declarations: [AppComponent, ModalComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
