e99f2ba65693b0a0c7ea09cbd1a4fa9d242420d4
Natalia Smalii <natali1gracia@gmail.com>
2020-12-07 10:56:35 +0200

    learn module structure in Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,17 @@
-import {NgModule} from '@angular/core'
-import {RouterModule} from '@angular/router'
-import {HomePageComponent} from './home-page/home-page.component'
-import {AboutPageComponent} from './about-page/about-page.component'
-import {AboutExtraPageComponent} from './about-page/about-extra-page/about-extra-page.component'
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
-  imports: [RouterModule.forRoot([
-    {path: 'about', component: AboutPageComponent, children: [
-      {path: 'extra', component: AboutExtraPageComponent}
-    ]},
-    {path: '', component: HomePageComponent, pathMatch: 'full'}
-  ])],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot([
+      { path: '', component: HomePageComponent, pathMatch: 'full' },
+      {
+        path: 'about',
+        loadChildren: './about-page/about-page.module#AboutPageModule',
+      },
+    ]),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
